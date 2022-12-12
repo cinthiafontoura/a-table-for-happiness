96e4db3ffcd3fd7847e0bcc4045ded532f60e110
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-12-12 21:00:12 +0000

    Update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn atableforhappiness.wsgi:application --bind 0.0.0.0:${PORT}
+web: gunicorn atableforhappiness.wsgi.application
--- atableforhappiness/settings.py
+++ atableforhappiness/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['atableforhappiness.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['a-table-for-happiness.herokuapp.com', 'localhost']
 
 
 # Application definition
